693ddec754ec749874e4a3c8f1945bbc903f15d1
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2021-10-11 20:59:00 +0100

    Update nice-select2.js

--- src/js/nice-select2.js
+++ src/js/nice-select2.js
@@ -1,27 +1,17 @@
 import "../scss/nice-select2.scss";
 
 // utility functions
-function triggerClick(el) {
-  var event = document.createEvent("MouseEvents");
-  event.initEvent("click", true, false);
+function triggerEvent(el, event) {
+  if (!event) {
+    event = document.createEvent("MouseEvents");
+    event.initEvent("click", true, false);
+  }
   el.dispatchEvent(event);
 }
 
 function triggerChange(el) {
   var event = document.createEvent("HTMLEvents");
   event.initEvent("change", true, false);
-  el.dispatchEvent(event);
-}
-
-function triggerFocusIn(el) {
-  var event = document.createEvent("FocusEvent");
-  event.initEvent("focusin", true, false);
-  el.dispatchEvent(event);
-}
-
-function triggerFocusOut(el) {
-  var event = document.createEvent("FocusEvent");
-  event.initEvent("focusout", true, false);
   el.dispatchEvent(event);
 }
 
@@ -46,13 +36,22 @@ function removeClass(el, className) {
   if (el) return el.classList.remove(className);
 }
 
+function isScrolledIntoView(ele, container) {
+  const { bottom, height, top } = ele.getBoundingClientRect();
+  const containerRect = container.getBoundingClientRect();
+
+  return top >= containerRect.top && bottom <= containerRect.bottom;
+}
+
 var defaultOptions = {
   data: null,
-  searchable: false
+  searchable: false,
 };
+
 function NiceSelect(element, options) {
   this.el = element;
   this.config = Object.assign({}, defaultOptions, options || {});
+
   this.data = this.config.data;
   this.selectedOptions = [];
 
@@ -68,8 +67,9 @@ function NiceSelect(element, options) {
   this.create();
 }
 
-NiceSelect.prototype.create = function() {
+NiceSelect.prototype.create = function () {
   this.el.style.display = "none";
+
   if (this.data) {
     this.processData(this.data);
   } else {
@@ -80,45 +80,38 @@ NiceSelect.prototype.create = function() {
   this.bindEvent();
 };
 
-NiceSelect.prototype.processData = function(data) {
+NiceSelect.prototype.processData = function (data) {
   var options = [];
-  data.forEach(item=> {
+  data.forEach(function (item) {
     options.push({
       data: item,
       attributes: {
         selected: false,
         disabled: false,
-		optgroup: item.value == 'optgroup'
-      }
+        display: true,
+      },
     });
   });
   this.options = options;
 };
 
-NiceSelect.prototype.extractData = function() {
-  var options = this.el.querySelectorAll("option,optgroup");
+NiceSelect.prototype.extractData = function () {
+  var options = this.el.querySelectorAll("option");
   var data = [];
   var allOptions = [];
   var selectedOptions = [];
 
-  options.forEach(item => {
-	if(item.tagName == 'OPTGROUP'){
-		var itemData = {
-		  text: item.label,
-		  value: 'optgroup'
-		};
-	}else{
-		var itemData = {
-		  text: item.innerText,
-		  value: item.value
-		};
-
-	}
+  options.forEach((item) => {
+    var itemData = {
+      text: item.innerText,
+      value: item.value,
+    };
 
     var attributes = {
-      selected: item.getAttribute("selected") != null,
+      selected:
+        item.getAttribute("selected") != null || item.value == this.el.value,
       disabled: item.getAttribute("disabled") != null,
-	  optgroup: item.tagName == 'OPTGROUP'
+      display: true,
     };
 
     data.push(itemData);
@@ -127,19 +120,19 @@ NiceSelect.prototype.extractData = function() {
 
   this.data = data;
   this.options = allOptions;
-  this.options.forEach(function(item) {
+  this.options.forEach(function (item) {
     if (item.attributes.selected) selectedOptions.push(item);
   });
 
   this.selectedOptions = selectedOptions;
 };
 
-NiceSelect.prototype.renderDropdown = function() {
+NiceSelect.prototype.renderDropdown = function () {
   var classes = [
     "nice-select",
     attr(this.el, "class") || "",
     this.disabled ? "disabled" : "",
-    this.multiple ? "has-multiple" : ""
+    this.multiple ? "has-multiple" : "",
   ];
 
   let searchHtml = `<div class="nice-select-search-box">
@@ -163,18 +156,15 @@ NiceSelect.prototype.renderDropdown = function() {
   this._renderItems();
 };
 
-NiceSelect.prototype._renderSelectedItems = function() {
+NiceSelect.prototype._renderSelectedItems = function () {
   if (this.multiple) {
     var selectedHtml = "";
-	if(window.getComputedStyle(this.dropdown).width == 'auto' || this.selectedOptions.length <2){
-		this.selectedOptions.forEach(function(item) {
-		  selectedHtml += `<span class="current">${item.data.text}</span>`;
-		});
-		selectedHtml = selectedHtml == "" ? this.placeholder : selectedHtml;
-	}else{
-		selectedHtml = 'Multiple';
-	}
-	
+
+    this.selectedOptions.forEach(function (item) {
+      selectedHtml += `<span class="current">${item.data.text}</span>`;
+    });
+    selectedHtml = selectedHtml == "" ? this.placeholder : selectedHtml;
+
     this.dropdown.querySelector(".multiple-options").innerHTML = selectedHtml;
   } else {
     var html =
@@ -186,35 +176,36 @@ NiceSelect.prototype._renderSelectedItems = function() {
   }
 };
 
-NiceSelect.prototype._renderItems = function() {
+NiceSelect.prototype._renderItems = function () {
   var ul = this.dropdown.querySelector("ul");
-  this.options.forEach(item => {
-    ul.appendChild(this._renderItem(item));
+  ul.scrollTop = 0;
+  ul.innerHTML = "";
+  this.options.forEach((item) => {
+    if (item.display) {
+      ul.appendChild(this._renderItem(item));
+    }
   });
 };
 
-NiceSelect.prototype._renderItem = function(option) {
+NiceSelect.prototype._renderItem = function (option) {
   var el = document.createElement("li");
-  el.innerHTML = option.data.text;
-  if(option.attributes.optgroup){
-	  el.classList.add('optgroup');
-  }else{
- 	el.setAttribute("data-value", option.data.value);
-	var classList = [
-		"option",
-		option.attributes.selected ? "selected" : null,
-		option.attributes.disabled ? "disabled" : null,
-	];
-	
-	el.addEventListener("click", this._onItemClicked.bind(this, option));
-	el.classList.add(...classList);
-  }
+  el.setAttribute("data-value", option.data.value);
 
+  var classList = [
+    "option",
+    option.attributes.selected ? "selected" : null,
+    option.attributes.disabled ? "disabled" : null,
+  ];
+
+  el.classList.add(...classList);
+  el.innerHTML = option.data.text;
+  el.addEventListener("click", (e) => this._onItemSelected(option, e));
+  el.addEventListener("select", (e) => this._onItemSelected(option, e));
   option.element = el;
   return el;
 };
 
-NiceSelect.prototype.update = function() {
+NiceSelect.prototype.update = function () {
   this.extractData();
   if (this.dropdown) {
     var open = hasClass(this.dropdown, "open");
@@ -222,69 +213,14 @@ NiceSelect.prototype.update = function() {
     this.create();
 
     if (open) {
-      triggerClick(this.dropdown);
+      triggerEvent(this.dropdown);
     }
   }
 };
 
-NiceSelect.prototype.disable = function() {
-  if (!this.disabled) {
-    this.disabled = true;
-    addClass(this.dropdown, "disabled");
-  }
-};
-
-NiceSelect.prototype.enable = function() {
-  if (this.disabled) {
-    this.disabled = false;
-    removeClass(this.dropdown, "disabled");
-  }
-};
-
-NiceSelect.prototype.clear = function() {
-  this.selectedOptions = [];
-  this._renderSelectedItems();
-  this.updateSelectValue();
-  triggerChange(this.el);
-};
-
-NiceSelect.prototype.destroy = function() {
-  if (this.dropdown) {
-    this.dropdown.parentNode.removeChild(this.dropdown);
-    this.el.style.display = "";
-  }
-};
-
-NiceSelect.prototype.bindEvent = function() {
-  var $this = this;
-  this.dropdown.addEventListener("click", this._onClicked.bind(this));
-  this.dropdown.addEventListener("keydown", this._onKeyPressed.bind(this));
-  this.dropdown.addEventListener("focusin", triggerFocusIn.bind(this, this.el));
-  this.dropdown.addEventListener("focusout", triggerFocusOut.bind(this, this.el));
-  window.addEventListener("click", this._onClickedOutside.bind(this));
-
-  if (this.config.searchable) {
-    this._bindSearchEvent();
-  }
-};
-
-NiceSelect.prototype._bindSearchEvent = function() {
-  var searchBox = this.dropdown.querySelector(".nice-select-search");
-  if (searchBox)
-    searchBox.addEventListener("click", function(e) {
-      e.stopPropagation();
-      return false;
-    });
-
-  searchBox.addEventListener("input", this._onSearchChanged.bind(this));
-};
-
-NiceSelect.prototype._onClicked = function(e) {
-	if (this.multiple) {
-		this.dropdown.classList.add("open");
-	}else{
-		this.dropdown.classList.toggle("open");
-	}
+NiceSelect.prototype.focus = function () {
+  this.dropdown.classList.toggle("open");
+  this.dropdown.querySelector("ul").scrollTop = 0;
 
   if (this.dropdown.classList.contains("open")) {
     var search = this.dropdown.querySelector(".nice-select-search");
@@ -297,29 +233,82 @@ NiceSelect.prototype._onClicked = function(e) {
     removeClass(t, "focus");
     t = this.dropdown.querySelector(".selected");
     addClass(t, "focus");
-    this.dropdown.querySelectorAll("ul li").forEach(function(item) {
-      item.style.display = "";
-    });
+    for (let i = 0; i < this.options.length; i++) {
+      this.options[i].display = true;
+    }
+    this._renderItems();
   } else {
     this.dropdown.focus();
   }
 };
 
-NiceSelect.prototype._onItemClicked = function(option, e) {
+NiceSelect.prototype.disable = function () {
+  if (!this.disabled) {
+    this.disabled = true;
+    addClass(this.dropdown, "disabled");
+  }
+};
+
+NiceSelect.prototype.enable = function () {
+  if (this.disabled) {
+    this.disabled = false;
+    removeClass(this.dropdown, "disabled");
+  }
+};
+
+NiceSelect.prototype.clear = function () {
+  this.selectedOptions = [];
+  this._renderSelectedItems();
+  this.updateSelectValue();
+  triggerChange(this.el);
+};
+
+NiceSelect.prototype.destroy = function () {
+  if (this.dropdown) {
+    this.dropdown.parentNode.removeChild(this.dropdown);
+    this.el.style.display = "";
+  }
+};
+
+NiceSelect.prototype.bindEvent = function () {
+  var $this = this;
+  this.dropdown.addEventListener("click", this._onClicked.bind(this));
+  this.dropdown.addEventListener("keydown", this._onKeyPressed.bind(this));
+  window.addEventListener("click", this._onClickedOutside.bind(this));
+
+  if (this.config.searchable) {
+    this._bindSearchEvent();
+  }
+};
+
+NiceSelect.prototype._bindSearchEvent = function () {
+  var searchBox = this.dropdown.querySelector(".nice-select-search");
+  if (searchBox)
+    searchBox.addEventListener("click", function (e) {
+      e.stopPropagation();
+      return false;
+    });
+
+  searchBox.addEventListener("input", this._onSearchChanged.bind(this));
+};
+
+NiceSelect.prototype._onClicked = function (e) {
+  this.focus();
+};
+
+NiceSelect.prototype._onItemSelected = function (option, e) {
+  e = e.detail || e;
   var optionEl = e.target;
 
   if (!hasClass(optionEl, "disabled")) {
+    this.dropdown.classList.remove("open");
     if (this.multiple) {
-      if (hasClass(optionEl, "selected")) {
-		removeClass(optionEl, "selected");
-		this.selectedOptions.splice(this.selectedOptions.indexOf(option),1);
-		this.el.querySelector('option[value="' + optionEl.dataset.value + '"]').selected=false;
-	  }else{
+      if (!hasClass(optionEl, "selected")) {
         addClass(optionEl, "selected");
         this.selectedOptions.push(option);
       }
     } else {
-      this.selectedOptions.forEach(function(item) {
+      this.selectedOptions.forEach(function (item) {
         removeClass(item.element, "selected");
       });
 
@@ -327,19 +316,25 @@ NiceSelect.prototype._onItemClicked = function(option, e) {
       this.selectedOptions = [option];
     }
 
+    this.dispatchOnChanged(e);
+
     this._renderSelectedItems();
     this.updateSelectValue();
   }
 };
 
-NiceSelect.prototype.updateSelectValue = function() {
+NiceSelect.prototype.dispatchOnChanged = function (e) {
+  if (this.config.onChanged) {
+    let values = this.selectedOptions.map((e) => e.data.value);
+    this.config.onChanged(values, e);
+  }
+};
+
+NiceSelect.prototype.updateSelectValue = function () {
   if (this.multiple) {
-    var select = this.el;
-    this.selectedOptions.forEach(function(item) {
-      var el = select.querySelector('option[value="' + item.data.value + '"]');
-      if (el){
-		  el.setAttribute("selected", true);
-	  }
+    this.selectedOptions.each(function (item) {
+      var el = this.el.querySelector('option[value="' + item.data.value + '"]');
+      if (el) el.setAttribute("selected", true);
     });
   } else if (this.selectedOptions.length > 0) {
     this.el.value = this.selectedOptions[0].data.value;
@@ -347,60 +342,69 @@ NiceSelect.prototype.updateSelectValue = function() {
   triggerChange(this.el);
 };
 
-NiceSelect.prototype._onClickedOutside = function(e) {
+NiceSelect.prototype._onClickedOutside = function (e) {
   if (!this.dropdown.contains(e.target)) {
     this.dropdown.classList.remove("open");
   }
 };
 
-NiceSelect.prototype._onKeyPressed = function(e) {
+NiceSelect.prototype._onKeyPressed = function (e) {
+  e.preventDefault();
   // Keyboard events
-
   var focusedOption = this.dropdown.querySelector(".focus");
 
   var open = this.dropdown.classList.contains("open");
 
   // Space or Enter
-  if (e.keyCode == 32 || e.keyCode == 13) {
+  if (e.keyCode == 13) {
     if (open) {
-      triggerClick(focusedOption);
+      let event = new CustomEvent("select", {
+        detail: e,
+      });
+      triggerEvent(focusedOption, event);
     } else {
-      triggerClick(this.dropdown);
+      triggerEvent(this.dropdown);
     }
   } else if (e.keyCode == 40) {
     // Down
     if (!open) {
-      triggerClick(this.dropdown);
+      triggerEvent(this.dropdown);
     } else {
       var next = this._findNext(focusedOption);
       if (next) {
         var t = this.dropdown.querySelector(".focus");
         removeClass(t, "focus");
         addClass(next, "focus");
+        if (!isScrolledIntoView(next, next.parentElement)) {
+          next.parentElement.scrollTop += next.offsetHeight;
+        }
       }
     }
     e.preventDefault();
   } else if (e.keyCode == 38) {
     // Up
     if (!open) {
-      triggerClick(this.dropdown);
+      triggerEvent(this.dropdown);
     } else {
       var prev = this._findPrev(focusedOption);
       if (prev) {
         var t = this.dropdown.querySelector(".focus");
         removeClass(t, "focus");
         addClass(prev, "focus");
+        if (!isScrolledIntoView(prev, prev.parentElement)) {
+          prev.parentElement.scrollTop -= prev.offsetHeight;
+        }
       }
     }
     e.preventDefault();
   } else if (e.keyCode == 27 && open) {
     // Esc
-    triggerClick(this.dropdown);
+    triggerEvent(this.dropdown);
   }
   return false;
 };
 
-NiceSelect.prototype._findNext = function(el) {
+NiceSelect.prototype._findNext = function (el) {
   if (el) {
     el = el.nextElementSibling;
   } else {
@@ -417,7 +421,7 @@ NiceSelect.prototype._findNext = function(el) {
   return null;
 };
 
-NiceSelect.prototype._findPrev = function(el) {
+NiceSelect.prototype._findPrev = function (el) {
   if (el) {
     el = el.previousElementSibling;
   } else {
@@ -434,25 +438,28 @@ NiceSelect.prototype._findPrev = function(el) {
   return null;
 };
 
-NiceSelect.prototype._onSearchChanged = function(e) {
+NiceSelect.prototype._onSearchChanged = function (e) {
+  e.preventDefault();
   var open = this.dropdown.classList.contains("open");
   var text = e.target.value;
   text = text.toLowerCase();
 
   if (text == "") {
-    this.options.forEach(function(item) {
-      item.element.style.display = "";
+    this.options.forEach(function (item) {
+      item.display = true;
     });
+    this._renderItems();
   } else if (open) {
     var matchReg = new RegExp(text);
-    this.options.forEach(function(item) {
+    this.options.forEach(function (item) {
       var optionText = item.data.text.toLowerCase();
       var matched = matchReg.test(optionText);
-      item.element.style.display = matched ? "" : "none";
+      item.display = matched;
     });
+    this._renderItems();
   }
 
-  this.dropdown.querySelectorAll(".focus").forEach(function(item) {
+  this.dropdown.querySelectorAll(".focus").forEach(function (item) {
     removeClass(item, "focus");
   });
 
